7093800a2d208469031baeec72dba4b563650d85
Henry Farmery <jhrf2@cam.ac.uk>
2014-12-17 19:28:43 +0000

    Added a setup.py file.

---
+++ setup.py
@@ -0,0 +1,8 @@
+setup(name='parabam-docs',
+	  description='Documents for parabam',
+      version='0.1.1dev',
+      author="JHR Farmery",
+      license='BSD',
+      author_email = 'jhrf2@cam.ac.uk',
+      requires = ['parabam','sphinxcontrib.napoleon','sphinx'],
+      )
